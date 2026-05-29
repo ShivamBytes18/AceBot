dd2fabef67f96bf061cc54b6945b5e22b5e3dfa2
ShivamBytes18 <su9555181178@gmail.com>
2026-05-29 19:24:20 +0530

    Auth Controller

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,27 @@
+import genToken from "../config/token.js"
+import User from "../models/user.model.js"
+
+export const googleAuth = async (req, res) => {
+    try {
+        const {name , email }= req.body
+        let user = await User.findOne({email})
+        if(!user)
+            {
+                user = await User.create({
+                    name,
+                    email
+                })
+            } 
+            let token = await genToken(user._id)
+            res.cookie("token",token,{
+                http:true,
+                secure:false,
+                sameSite:"strict",
+                maxAg:7*24*60*60*1000
+            })
+
+            return res.status(200).json(user)
+    } catch (error) {
+        return res.status(500).json({message:`google Auth Error :${error}`})
+    }
+}
