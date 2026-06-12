3e037f366cd0804b3eb0fab201e8e909d83f6d0b
ShivamBytes18 <su9555181178@gmail.com>
2026-06-12 23:35:48 +0530

    Added Functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -59,7 +59,7 @@ function Navbar() {
         }}
         className='flex items-center gap-2 bg-gray-100
         px-4 py-2 rounded-full text-md hover:bg-gray-200 transition'>
-          <BsCoin size={20}/>
+          <BsCoin size={20.05}/>
           {userData?.credits || 0}
         </button>
         {showCreditPopup && (
